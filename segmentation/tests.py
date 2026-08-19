2a2e3f6ba080c3c376b92aa710c65bfa73da72dd
JETTIGARI LOKESH GOUD <jkittuloke@gmail.com>
2026-08-19 12:36:32 +0530

    Add Django application tests

--- segmentation/tests.py
+++ segmentation/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+
+class SegmentationPageTests(TestCase):
+
+    def test_home_page_loads(self):
+        response = self.client.get(reverse("home"))
+        self.assertEqual(response.status_code, 200)
+
+    def test_home_page_contains_title(self):
+        response = self.client.get(reverse("home"))
+        self.assertContains(response, "Retinal")
